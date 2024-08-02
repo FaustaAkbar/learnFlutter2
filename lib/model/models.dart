38f02521bee71b5c65be7e9ceee0148139fd8c00
FaustaAkbar <faussss1357@gmail.com>
2024-08-02 20:26:56 +0700

    [Model] This is for model project
    I am forget to checkout my branch at branch master, so this is branch master but onesly this is branch master

---
+++ lib/model/models.dart
@@ -0,0 +1,11 @@
+class myModel {
+  String url;
+  String nama;
+  int harga;
+  String deskripsi;
+  myModel(
+      {required this.url,
+      required this.nama,
+      required this.harga,
+      required this.deskripsi});
+}
