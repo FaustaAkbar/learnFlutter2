d942490a2b1abc5789723d617f958ae6ddc621e3
FaustaAkbar <faussss1357@gmail.com>
2024-08-03 12:13:00 +0700

    [initial] Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,49 @@
-import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
-import 'model/models.dart';
-import 'dart:math';
 
 void main() {
-  runApp(Myapp());
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Homepage(),
+      theme: ThemeData(brightness: Brightness.dark),
+      home: MyHomepage(),
     );
   }
 }
 
-class Homepage extends StatelessWidget {
-  final Faker faker = Faker();
-
+class MyHomepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<myModel> dummyData = List.generate(20, (index) {
-      return myModel(
-          url: "https://picsum.photos/id/$index/300",
-          nama: faker.food.dish(),
-          harga: Random().nextInt(1000),
-          deskripsi: faker.lorem.sentence());
-    });
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
-          "Product",
-          style: TextStyle(fontSize: 40),
+        title: Text("Theme"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("This is text 1"),
+            SizedBox(
+              height: 10,
+            ),
+            Text("This is text 2"),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Submit"))
+          ],
         ),
       ),
-      body: GridView.builder(
-          padding: EdgeInsets.all(10),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
-          itemBuilder: (expect, index) {
-            return GridTile(
-              child: Image.network(dummyData[index].url),
-              footer: Container(
-                height: 80,
-                alignment: Alignment.center,
-                color: Colors.green.shade300,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      dummyData[index].nama,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "Harga: ${dummyData[index].harga}",
-                      style: TextStyle(color: Colors.red),
-                    ),
-                    Text(
-                      textAlign: TextAlign.center,
-                      dummyData[index].deskripsi,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.home),
+      ),
     );
   }
 }
