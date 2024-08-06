2badd648ecef437ee7144bcd24da1bd6cbdab0ea
FaustaAkbar <faussss1357@gmail.com>
2024-08-06 15:15:12 +0700

    [Final] final Flexibel and expanded

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,55 @@
-import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
-import 'model/models.dart';
-import 'dart:math';
 
 void main() {
-  runApp(Myapp());
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Homepage(),
+      theme: ThemeData(appBarTheme: AppBarTheme(backgroundColor: Colors.blue)),
+      home: MyHomepage(),
     );
   }
 }
 
-class Homepage extends StatelessWidget {
-  final Faker faker = Faker();
-
+class MyHomepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<myModel> dummyData = List.generate(20, (index) {
-      return myModel(
-          url: "https://picsum.photos/id/$index/300",
-          nama: faker.food.dish(),
-          harga: Random().nextInt(1000),
-          deskripsi: faker.lorem.sentence());
-    });
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
-          "Product",
-          style: TextStyle(fontSize: 40),
-        ),
+        title: Text("Flexibel and Exspanded"),
       ),
-      body: GridView.builder(
-          padding: EdgeInsets.all(10),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
-          itemBuilder: (expect, index) {
-            return GridTile(
-              child: Image.network(dummyData[index].url),
-              footer: Container(
-                height: 80,
-                alignment: Alignment.center,
-                color: Colors.green.shade300,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      dummyData[index].nama,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "Harga: ${dummyData[index].harga}",
-                      style: TextStyle(color: Colors.red),
-                    ),
-                    Text(
-                      textAlign: TextAlign.center,
-                      dummyData[index].deskripsi,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }),
+      body: Row(
+        children: [
+          Flexible(
+            fit: FlexFit.loose,
+            child: Container(
+              height: 100,
+              color: Colors.red,
+              child: Text("Flexible 1"),
+            ),
+          ),
+          Flexible(
+            fit: FlexFit.loose,
+            child: Container(
+              height: 100,
+              color: Colors.amber,
+              child: Text("Flexible 2"),
+            ),
+          ),
+          Expanded(
+              child: Container(
+            height: 100,
+            color: Colors.blue,
+            child: Text("Hai"),
+          ))
+        ],
+      ),
     );
   }
 }
