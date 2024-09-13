73a032608957f657145ac1bf3f1387b8723c2d09
FaustaAkbar <faussss1357@gmail.com>
2024-09-13 23:18:49 +0700

    [Final]

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
-import 'package:application2/providers/all_Products.dart';
-import 'package:application2/providers/chart.dart';
-import 'package:application2/screens/card_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import './screens/products_overview_screen.dart';
-import './screens/product_detail_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,29 +8,80 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => Products()),
-        ChangeNotifierProvider(create: (context) => Chart())
-      ],
-      child: MaterialApp(
+    return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'MyShop',
         theme: ThemeData(
-            primarySwatch: Colors.indigo,
-            primaryColor: Colors.amber,
-            fontFamily: 'Lato',
             appBarTheme: AppBarTheme(
-              backgroundColor: Colors.blue,
-              titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
-              centerTitle: true,
-            )),
-        home: ProductsOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          CardScreen.routeName: (ctx) => CardScreen(),
-        },
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        )),
+        home: /* ChangeNotifierProvider(
+          create: (context) => Counter(),
+          child: Homepage(),
+        ) */
+            MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (context) => Counter(),
+            )
+          ],
+          child: Homepage(),
+        ));
+  }
+}
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final counterr = Provider.of<Counter>(context, listen: false);
+    print("Render");
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Provider"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Consumer<Counter>(
+            builder: (context, value, child) => Text(
+              "${value._jumlah}",
+              style: TextStyle(fontSize: 100),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: counterr.add,
+                icon: Icon(Icons.add),
+              ),
+              IconButton(
+                onPressed: counterr.minus,
+                icon: Icon(Icons.remove),
+              )
+            ],
+          )
+        ],
       ),
     );
+  }
+}
+
+class Counter with ChangeNotifier {
+  int _jumlah = 0;
+  int get jumlah => _jumlah;
+
+  void add() {
+    _jumlah += 1;
+    notifyListeners();
+  }
+
+  void minus() {
+    _jumlah -= 1;
+    notifyListeners();
   }
 }
